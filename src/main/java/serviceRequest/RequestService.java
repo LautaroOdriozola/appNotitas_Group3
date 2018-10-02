93e585bcc5112aadcb27419d8ad28b4393b0d235
NaylaWinter <naylawinter@gmail.com>
2018-10-02 10:07:22 -0300

    Cambios en el service request

--- src/main/java/serviceRequest/RequestService.java
+++ src/main/java/serviceRequest/RequestService.java
@@ -6,15 +6,14 @@ import com.sun.jersey.api.client.WebResource;
 import javax.ws.rs.core.MediaType;
 
 /* Recursos a utilizar
-*  http://notitas.herokuapp.com/PUT/student 
-*  http://notitas.herokuapp.com/GET/student
-*  http://notitas.herokuapp.com/GET/student/assignments
+*  http://notitas.herokuapp.com/student (put)
+*  http://notitas.herokuapp.com/student (get)
+*  http://notitas.herokuapp.com/student/assignments (get)
 * */
 
 public class RequestService {
     private Client client;
-    private static final String API_NOTITAS_GET = "hhttp://notitas.herokuapp.com/GET";
-    private static final String API_NOTITAS_PUT = "hhttp://notitas.herokuapp.com/PUT";
+    private static final String API_NOTITAS = "hhttp://notitas.herokuapp.com/GET";
     private static final String RESOURCE_STUDENT = "student";
     private static final String RESOURCE_ASSIGMENTS = "student/assignments";
 
@@ -23,22 +22,34 @@ public class RequestService {
         this.client = Client.create();
     }
 
-    public String getDatosAlumno(){
-        ClientResponse responseDatosAlumno = this.client.resource(API_NOTITAS_GET).path(RESOURCE_STUDENT)
-                .accept(MediaType.APPLICATION_JSON)
+    public String getDatosAlumno(String body, String token){
+        ClientResponse responseDatosAlumno = this.client.resource(API_NOTITAS).path(RESOURCE_STUDENT)
+        		.header("Authorization", token)
+                .accept("application/json")
                 .get(ClientResponse.class);
         
         String datosAlumnoEnString = responseDatosAlumno.getEntity(String.class);
         return datosAlumnoEnString;
+        
     }
     
     public String getDatosAsignaciones(){
         ClientResponse responseDatosAsignaciones = this.client.resource(API_NOTITAS_GET).path(RESOURCE_ASSIGMENTS)
-                .accept(MediaType.APPLICATION_JSON)
+        		.header("Authorization", /TOKEN/)
+                .accept("application/json")
                 .get(ClientResponse.class);
         
         String datosAsigEnString = responseDatosAsignaciones.getEntity(String.class);
         return datosAsigEnString;
+    }
+    
+    
+    public void putDatosAlumnos(String body, String token) {
+    WebResource webResource = client.resource();
+    ClientResponse response = webResource.resource(API_NOTITAS).path(RESOURCE_STUDENT)
+                    .header("Authorization", token)
+                    .accept("application/json")
+                    .put(ClientResponse.class, body);
     }
 
    
